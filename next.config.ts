c58d4865d660c62271f2bd37490b7c9a8d136119
Servando Reyes <l1007650@bancogalicia.com.ar>
2025-01-26 02:39:19 -0300

    next config github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/biajamo_lp",
+  assetPrefix: "/biajamo_lp/",
+  trailingSlash: true,
+  output: 'export', // Enable static export
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
 };
 
 export default nextConfig;
